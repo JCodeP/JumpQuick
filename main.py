3a0e88a0fa40024a7c31311bbee36fab6ec27f5f
JCodeP <78876010+JCodeP@users.noreply.github.com>
2025-02-25 04:53:55 -0600

    initial pygame setup

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+
+
+pygame.init()
+
+mainScreen = pygame.display.set_mode((800, 600))
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    mainScreen.fill((255, 255, 255))
+
+    pygame.display.flip()
+pygame.quit()
